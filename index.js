2e62591f87e94ff5d856c3d8a0c929ad8bbb763a
Ajanitone <ajanitone@freenet.de>
2022-08-17 14:34:54 +0200

    pb-data-struct-arraymethod-filter

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 //Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.
 //1
+let arr = [5, 3, 8, 1];
+//write your code here
+const filterRange = (arr, a, b) =>
+  arr.filter((number) => number >= a && number <= b);
 
-function filterRange(arr, a, b) {
-    //write your code here
-  }
-  
-  let arr = [5, 3, 8, 1];
-  
-  let filtered = filterRange(arr, 1, 4);
-  
-  console.log( filtered ); // 3,1 (matching values)
-  
-  console.log( arr ); // 5,3,8,1 (not modified)
+let filtered = filterRange(arr, 5, 8);
+
+console.log("Q1:", filtered); // 3,1 (matching values)
+
+console.log("-----------------------------------------------");
 
 //Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
 //2
-  function filterRangeInPlace(arr, a, b) {
-    //write your code here
-  }
-  
-  let array = [5, 3, 8, 1];
-  
-  filterRangeInPlace(array, 1, 4); // removed the numbers except from 1 to 4
-  
-  console.log( array ); // [3, 1]
+const filterRangeInPlace = (arr, a, b) =>
+  array.filter((number) => number >= a && number <= b);
+
+let array = [5, 3, 8, 1];
+
+console.log("Q2:", filterRangeInPlace(array, 1, 4)); // [3, 1]
